0675aa16a7673ac6bbedae70c9522080d9519af5
Iván Barroso <ibarrosj@everis.com>
2021-04-14 16:16:59 +0200

    Actualizacion

--- src/json/GrabarFichero.java
+++ src/json/GrabarFichero.java
@@ -2,9 +2,15 @@ package json;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CodingErrorAction;
+import java.nio.charset.StandardCharsets;
 
 public class GrabarFichero {
 
@@ -31,8 +37,20 @@ public class GrabarFichero {
         }
         f = new File(nombre);
         w = new FileWriter(f);
+        
+//        CharsetEncoder encoder = Charset.forName("Windows-1252").newEncoder(); 
+//        encoder.onMalformedInput(CodingErrorAction.REPORT); 
+//        encoder.onUnmappableCharacter(CodingErrorAction.REPORT); 
+//        
+//
+//
+//        bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f),encoder)); 
+        
+        
         bw = new BufferedWriter(w);
         wr = new PrintWriter(bw);
+        
+        
     }
 
     public void agregarAFichero(String contenido) throws IOException {
--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,7 @@ import json.GenerarPostman;
 
 public class Main {
 
-    static String NOMBRE_FICHERO_ENTRADA_CORREOS_FASE21 = "correos/correoTodos10mar.txt";
+    static String NOMBRE_FICHERO_ENTRADA_CORREOS_FASE21 = "correos/correoTodos06abr.txt";
     static String NOMBRE_FICHERO_ENTRADA_PLANTILLA_POSTMAN = "plantilla/plantilla.json";
 
     public static void main(String[] args) throws IOException {
@@ -19,22 +19,24 @@ public class Main {
     	//Borrar contenido y directorio Colecciones
     	BorrarDirectorio.eliminarDirectorioYFicheros(new File("Colecciones"));	
     	
-    	
+    	boolean generarTest = true;
         GenerarPostman generarPostman = new GenerarPostman();
         TreeSet<String> listadoLLamadasJson = 
                 generarPostman.obtenerLlamadasJson(NOMBRE_FICHERO_ENTRADA_CORREOS_FASE21);
 
-//        generarPostman.obtenerSalida(NOMBRE_FICHERO_ENTRADA_PLANTILLA_POSTMAN, listadoLLamadasJson,
-//                GenerarPostman.FASE1, true);
-//        generarPostman.obtenerSalida(NOMBRE_FICHERO_ENTRADA_PLANTILLA_POSTMAN, listadoLLamadasJson,
-//                GenerarPostman.FASE2, true);
-//        generarPostman.obtenerSalida(NOMBRE_FICHERO_ENTRADA_PLANTILLA_POSTMAN, listadoLLamadasJson,
-//                GenerarPostman.FASE3, true);
         generarPostman.obtenerSalida(NOMBRE_FICHERO_ENTRADA_PLANTILLA_POSTMAN, listadoLLamadasJson,
-                GenerarPostman.FASE4, false);
+                GenerarPostman.FASE1, generarTest);
+        generarPostman.obtenerSalida(NOMBRE_FICHERO_ENTRADA_PLANTILLA_POSTMAN, listadoLLamadasJson,
+                GenerarPostman.FASE2, generarTest);
+        generarPostman.obtenerSalida(NOMBRE_FICHERO_ENTRADA_PLANTILLA_POSTMAN, listadoLLamadasJson,
+                GenerarPostman.FASE3, generarTest);
+        generarPostman.obtenerSalida(NOMBRE_FICHERO_ENTRADA_PLANTILLA_POSTMAN, listadoLLamadasJson,
+                GenerarPostman.FASE4, generarTest);
+        generarPostman.obtenerSalida(NOMBRE_FICHERO_ENTRADA_PLANTILLA_POSTMAN, listadoLLamadasJson,
+                GenerarPostman.FASE5, generarTest);
         
 //        generarPostman.obtenerSalida(NOMBRE_FICHERO_ENTRADA_PLANTILLA_POSTMAN, listadoLLamadasJson,
-//                GenerarPostman.FASE_ERROR2, true);
+//                GenerarPostman.FASE_ERROR, true);
         
 
     }
